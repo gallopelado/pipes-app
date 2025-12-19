cb620da918bc35867c96cb83a872279c2d8f3ef3
Tío Gallo <juanftp100@gmail.com>
2025-12-19 15:32:00 -0300

    preparando el espacio para los idiomas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// aqui va el locale para español
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+// idioma español, incluir LOCALE_ID del angular/core
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-PY';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es' } // Configurar el idioma predeterminado a español
+  ]
 };
--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
@@ -11,5 +11,19 @@ export default class BasicPageComponent {
   nameLower = signal('juan josé');
   nameUpper = signal('JUAN JOSÉ');
   fullName = signal('jUaN joSÉ')
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
+
 
 }
