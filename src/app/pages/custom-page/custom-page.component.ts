f5697f11c899fa2141ba00a2d0c9f144426b64fb
Tío Gallo <juanftp100@gmail.com>
2025-12-22 16:22:31 -0300

    cambiar el estado de un pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+
+  name = signal('Juan José');
+
+  upperCase = signal(true);
+
+  changeUpperToLower() {
+    if ( this.upperCase() ) {
+      this.upperCase.set(false);
+      return;
+    }
+
+    if ( !this.upperCase() ) {
+      this.upperCase.set(true);
+    }
+  }
+
+}
